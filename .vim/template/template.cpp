004dd160cc3cbe8d7a2ec67530490547cdbb0b24
Rapiz <rapiz@foxmail.com>
2019-08-22 21:31:33 +0800

    update

---
+++ .vim/template/template.cpp
@@ -0,0 +1,15 @@
+/**
+ *     ____              _    
+ *    / __ \____ _____  (_)___
+ *   / /_/ / __ `/ __ \/ /_  /
+ *  / _, _/ /_/ / /_/ / / / /_
+ * /_/ |_|\__,_/ .___/_/ /___/
+ *            /_/             
+ *           code@rapiz.me
+ */
+#include <bits/stdc++.h>
+#define xxx(x) cerr<<(#x)<<": "<<x<<endl;
+typedef long long ll;
+using namespace std;
+int main() {
+}
