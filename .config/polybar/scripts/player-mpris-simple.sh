0dbfb7868357dea41275f45fafb3588850310c2d
Rapiz <rapiz@foxmail.com>
2019-09-16 20:43:10 +0800

    update

--- .config/polybar/scripts/player-mpris-simple.sh
+++ .config/polybar/scripts/player-mpris-simple.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-player_status=$(playerctl status 2> /dev/null)
+player_status=$(playerctl status -i chrome,chroimum 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-    echo "ﱘ $(playerctl metadata title -i chrome,chromium)"
+    echo "ﱘ $(playerctl metadata title -i chrome,chromium 2>/dev/null)"
 elif [ "$player_status" = "Paused" ]; then
-    echo " $(playerctl metadata title -i chrome,chromium)"
+    echo " $(playerctl metadata title -i chrome,chromium 2>/dev/null)"
 else
     echo ""
 fi
