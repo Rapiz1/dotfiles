53c60145a80269da83eb95a204f504f7ecae0cc9
Rapiz <rapiz@foxmail.com>
2019-10-16 19:29:58 +0800

    update

--- Source/cmdlet/qqMsg.sh
+++ Source/cmdlet/qqMsg.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+i=0
 inotifywait -mq -e access /home/rapiz/Source/cmdlet/qqMsg.wav | while read event
 do
-notify-send 'QQ' '新消息' -a qqMsg
+i=$i+1
+if [[ $i -eq 4 ]]
+then
+	notify-send 'QQ' '新消息' -a qqMsg
+	i=0
+fi
 done
